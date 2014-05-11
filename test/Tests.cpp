f11bda078746e69b94f6a9b27df6a5896f1b101b
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2014-05-11 14:54:18 +0200

    Add initialization unit test

--- test/Tests.cpp
+++ test/Tests.cpp
@@ -1,7 +1,9 @@
 #include "gtest/gtest.h"
 
+#include "IMediaLibrary.h"
 
-TEST( MediaLibary, InsertFile )
+TEST( MediaLibary, Init )
 {
-
+    IMediaLibrary* ml = MediaLibraryFactory::create();
+    ASSERT_TRUE( ml->initialize( "test.db" ) );
 }
