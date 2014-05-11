1d34596e037e92f31d47b48f36604598ee9910cc
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2014-05-11 14:53:54 +0200

    Create tables when initializing

--- src/MediaLibrary.cpp
+++ src/MediaLibrary.cpp
@@ -1,4 +1,11 @@
 #include "MediaLibrary.h"
+#include "SqliteTools.h"
+#include "File.h"
+#include "Label.h"
+#include "Album.h"
+#include "AlbumTrack.h"
+#include "Show.h"
+#include "ShowEpisode.h"
 
 MediaLibrary::MediaLibrary()
 {
@@ -7,9 +14,19 @@ MediaLibrary::MediaLibrary()
 bool MediaLibrary::initialize(const std::string& dbPath)
 {
     int res = sqlite3_open( dbPath.c_str(), &m_dbConnection );
-    //FIXME:
     // PRAGMA foreign_keys = ON;
-    return res == SQLITE_OK;
+    if ( res != SQLITE_OK )
+        return false;
+    sqlite3_stmt* stmt;
+    sqlite3_prepare_v2( m_dbConnection, "PRAGMA foreign_keys = ON", -1, &stmt, NULL );
+    if ( sqlite3_step( stmt ) != SQLITE_DONE )
+        return false;
+    return ( File::createTable( m_dbConnection ) &&
+        Label::createTable( m_dbConnection ) &&
+        Album::createTable( m_dbConnection ) &&
+        AlbumTrack::createTable( m_dbConnection ) &&
+        Show::createTable( m_dbConnection ) &&
+        ShowEpisode::createTable( m_dbConnection ) );
 }
 
 
@@ -17,8 +34,8 @@ const std::vector<IFile*>&MediaLibrary::files()
 {
     if ( m_files == NULL )
     {
-        m_files = new std::vector<IFile*>();
-        // magic
+        const char* req = "SELECT * FROM Files";
+        SqliteTools::fetchAll<File>( m_dbConnection, req, 0, m_files );
     }
     return *m_files;
 }
