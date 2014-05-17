0bde9d01e18377a3f6dbcd6c16b268f4703db133
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2014-05-18 00:19:18 +0300

    SqliteTools: Fix memory leak

--- src/SqliteTools.cpp
+++ src/SqliteTools.cpp
@@ -4,17 +4,10 @@
 
 bool SqliteTools::createTable( sqlite3 *db, const char* request )
 {
-    sqlite3_stmt* stmt;
-    int res = sqlite3_prepare_v2( db, request, -1, &stmt, NULL );
-    if ( res != SQLITE_OK )
-    {
-        std::cerr << "Failed to execute request: " << request << std::endl;
-        std::cerr << sqlite3_errmsg( db ) << std::endl;
-        return false;
-    }
-    res = sqlite3_step( stmt );
+    auto stmt = executeRequest( db, request );
+    int res = sqlite3_step( stmt.get() );
     while ( res != SQLITE_DONE && res != SQLITE_ERROR )
-        res = sqlite3_step( stmt );
+        res = sqlite3_step( stmt.get() );
     if ( res == SQLITE_ERROR )
     {
         std::cerr << "Failed to execute request: " << request << std::endl;
