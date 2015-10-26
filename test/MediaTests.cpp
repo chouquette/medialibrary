82e153f4d75cb92c5164fe0f899f68e151cd0918
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-10-26 16:50:09 +0100

    tests: Media: Ensure default duration is -1

--- test/MediaTests.cpp
+++ test/MediaTests.cpp
@@ -48,6 +48,7 @@ TEST_F( Files, Create )
     ASSERT_EQ( f->showEpisode(), nullptr );
     ASSERT_TRUE( f->isStandAlone() );
     ASSERT_FALSE( f->isParsed() );
+    ASSERT_EQ( f->duration(), -1 );
 
     auto files = ml->files();
     ASSERT_EQ( files.size(), 1u );
