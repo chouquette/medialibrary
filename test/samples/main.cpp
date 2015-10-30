afb0d0397cd199ac8adbfc29fd0ed8287680fc1b
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-10-30 16:22:39 +0100

    tests: samples: Plugin a basic json test case description

--- test/samples/main.cpp
+++ test/samples/main.cpp
@@ -1,8 +1,10 @@
+#include <condition_variable>
 #include "gtest/gtest.h"
 #include <memory>
+#include <mutex>
+#include <rapidjson/document.h>
 
 #include "MediaLibrary.h"
-#include "Tester.h"
 
 static const char* testCases[] = {
     "simple",
@@ -19,26 +21,93 @@ class TestEnv : public ::testing::Environment
         }
 };
 
+class MockCallback : public IMediaLibraryCb
+{
+public:
+    MockCallback();
+    bool waitForParsingComplete();
+
+private:
+    void onFileAdded(MediaPtr) {}
+    void onFileUpdated(MediaPtr) {}
+    void onDiscoveryStarted(const std::string&) {}
+    void onDiscoveryCompleted(const std::string&) {}
+    void onReloadStarted() {}
+    void onReloadCompleted() {}
+    void onParsingStatsUpdated(uint32_t nbParsed, uint32_t nbToParse);
+
+    std::condition_variable m_parsingCompleteVar;
+    std::mutex m_parsingMutex;
+    bool m_done;
+};
+
 class Tests : public ::testing::TestWithParam<const char*>
 {
 protected:
-    std::unique_ptr<MediaLibrary> ml;
+    std::unique_ptr<MediaLibrary> m_ml;
+    std::unique_ptr<MockCallback> m_cb;
 
     virtual void SetUp() override
     {
-        ml.reset( new MediaLibrary );
-        ml->setVerbosity( LogLevel::Error );
+        m_cb.reset( new MockCallback );
+        m_ml.reset( new MediaLibrary );
+        m_ml->setVerbosity( LogLevel::Error );
+        m_ml->initialize( "test.db", "/tmp", m_cb.get() );
     }
+
     virtual void TearDown() override
     {
-        ml.reset();
+        m_ml.reset();
+        m_cb.reset();
     }
 };
 
+MockCallback::MockCallback()
+{
+    // Start locked. The locked will be released when waiting for parsing to be completed
+    m_parsingMutex.lock();
+}
+
+bool MockCallback::waitForParsingComplete()
+{
+    std::unique_lock<std::mutex> lock( m_parsingMutex, std::adopt_lock );
+    m_done = false;
+    // Wait for a while, generating snapshots can be heavy...
+    return m_parsingCompleteVar.wait_for( lock, std::chrono::seconds( 30 ), [this]() {
+        return m_done;
+    });
+}
+
+void MockCallback::onParsingStatsUpdated(uint32_t nbParsed, uint32_t nbToParse)
+{
+    if ( nbParsed == nbToParse && nbToParse > 0 )
+    {
+        std::lock_guard<std::mutex> lock( m_parsingMutex );
+        m_done = true;
+        m_parsingCompleteVar.notify_all();
+    }
+}
+
 TEST_P( Tests, Parse )
 {
-    std::unique_ptr<Tester> t( new Tester( ml.get(), GetParam() ) );
-    t->run();
+    auto casePath = std::string("testcases/") + GetParam() + ".json";
+    std::unique_ptr<FILE, int(*)(FILE*)> f( fopen( casePath.c_str(), "rb" ), &fclose );
+    ASSERT_NE( nullptr, f );
+    char buff[65536]; // That's how ugly I am!
+    auto ret = fread( buff, sizeof(buff[0]), sizeof(buff), f.get() );
+    ASSERT_NE( 0u, ret );
+    buff[ret] = 0;
+    rapidjson::Document doc;
+    doc.Parse( buff );
+
+    ASSERT_TRUE( doc.HasMember( "input" ) );
+    const auto& input = doc["input"];
+    for ( auto i = 0u; i < input.Size(); ++i )
+    {
+        std::cout << "Input folder" << input[i].GetString() << std::endl;
+        m_ml->discover( "media/" + std::string( input[i].GetString() ) );
+    }
+    m_cb->waitForParsingComplete();
 }
 
 INSTANTIATE_TEST_CASE_P(SamplesTests, Tests,
--- test/samples/samples.cmake
+++ test/samples/samples.cmake
@@ -1,9 +1,9 @@
 list(APPEND SAMPLE_TEST_SRCS
     samples/main.cpp
-    samples/Tester.cpp
 )
 
 add_executable(samples ${SAMPLE_TEST_SRCS})
+
 add_dependencies(samples gtest-dependency)
 target_link_libraries(samples medialibrary)
 target_link_libraries(samples gtest gtest_main)
