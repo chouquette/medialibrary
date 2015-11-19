1aa5630454db1918c079e07577c29aa2f57611e8
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2015-11-19 09:04:48 +0100

    Provide and use a FindRapidJson module

--- test/samples/samples.cmake
+++ test/samples/samples.cmake
@@ -3,6 +3,8 @@ list(APPEND SAMPLE_TEST_SRCS
     samples/Tester.cpp
 )
 
+find_package(rapidjson REQUIRED)
+
 add_executable(samples ${SAMPLE_TEST_SRCS})
 
 add_dependencies(samples gtest-dependency)
