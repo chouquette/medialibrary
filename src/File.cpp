56d6b513df9478bdfd2f37551fad56beb7912536
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-01-11 21:02:20 +0100

    File: Index media_id

--- src/File.cpp
+++ src/File.cpp
@@ -139,8 +139,11 @@ bool File::createTable( DBConnection dbConnection )
             " BEGIN"
             " UPDATE " + policy::FileTable::Name + " SET is_present = new.is_present WHERE folder_id = new.id_folder;"
             " END";
+    std::string indexReq = "CREATE INDEX IF NOT EXISTS file_media_id_index ON " +
+            policy::FileTable::Name + "(media_id)";
     return sqlite::Tools::executeRequest( dbConnection, req ) &&
-            sqlite::Tools::executeRequest( dbConnection, triggerReq );
+            sqlite::Tools::executeRequest( dbConnection, triggerReq ) &&
+            sqlite::Tools::executeRequest( dbConnection, indexReq );
 }
 
 std::shared_ptr<File> File::create( DBConnection dbConnection, unsigned int mediaId, Type type, const fs::IFile& fileFs, unsigned int folderId, bool isRemovable )
