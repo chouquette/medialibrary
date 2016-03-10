f76685166bbdc9dae9317ef20bab3dd3e24ffe9f
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-03-10 18:28:46 +0100

    fs: unix: Dir: Remove duplicated check.
    opendir will fail soon enough if we're trying to open something which
    isn't a directory

--- src/filesystem/unix/Directory.cpp
+++ src/filesystem/unix/Directory.cpp
@@ -38,14 +38,6 @@ namespace fs
 Directory::Directory( const std::string& path )
     : m_path( toAbsolute( path ) )
 {
-#ifndef NDEBUG
-    struct stat s;
-    if ( lstat( m_path.c_str(), &s ) != 0 )
-        throw std::runtime_error( "Failed to stat directory " + path + "(" +
-                                  strerror( errno ) + ")" );
-    if ( S_ISDIR( s.st_mode ) == false )
-        throw std::runtime_error( "The provided path isn't a directory" );
-#endif
     if ( *m_path.crbegin() != '/' )
         m_path += '/';
 }
