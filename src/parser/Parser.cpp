1be74eaee862045e2845b28e2c7a9cc9f6c9d223
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-12-16 14:18:22 +0100

    Parser: Remove leftover debug

--- src/parser/Parser.cpp
+++ src/parser/Parser.cpp
@@ -117,7 +117,6 @@ void Parser::updateStats()
     auto percent = m_opToDo > 0 ? ( m_opDone * 100 / m_opToDo ) : 0;
     if ( percent != m_percent )
     {
-        LOG_ERROR( "Parser progress: ", percent, '%' );
         m_percent = percent;
         m_callback->onParsingStatsUpdated( m_percent );
     }
