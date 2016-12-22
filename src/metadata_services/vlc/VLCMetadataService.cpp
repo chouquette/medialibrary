8784a8cffbcdea4271c4e7766ba0a491af3fcd53
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2016-12-22 13:53:29 +0100

    VLCMetadataService: Don't account for the current ParserStep
    This ParserService is only meant to preparse & wait for preparse
    completion. If another service (ie. the thumbnailer) already created the
    media, there is no use for this task to run.

--- src/metadata_services/vlc/VLCMetadataService.cpp
+++ src/metadata_services/vlc/VLCMetadataService.cpp
@@ -98,9 +98,7 @@ uint8_t VLCMetadataService::nbThreads() const
 bool VLCMetadataService::isCompleted( const parser::Task& task ) const
 {
     // We always need to run this task if the metadata extraction isn't completed
-    return ( static_cast<uint8_t>( task.file->parserStep() ) &
-            static_cast<uint8_t>( File::ParserStep::MetadataAnalysis ) ) != 0 &&
-            task.vlcMedia.isValid() == true;
+    return task.vlcMedia.isValid() == true;
 }
 
 }
