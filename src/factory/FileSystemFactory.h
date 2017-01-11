f79b5b8afd4ffda550a5055035058fac05ab8ffe
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-01-11 16:46:29 +0100

    FileSystemFactory: Remove leftover method declaration

--- src/factory/FileSystemFactory.h
+++ src/factory/FileSystemFactory.h
@@ -50,9 +50,6 @@ namespace factory
         virtual bool isNetworkFileSystem() const override;
 
     private:
-        std::string toLocalPath( const std::string& mrl );
-
-    private:
         std::unordered_map<std::string, std::shared_ptr<fs::IDirectory>> m_dirs;
         compat::Mutex m_mutex;
         DeviceListerPtr m_deviceLister;
