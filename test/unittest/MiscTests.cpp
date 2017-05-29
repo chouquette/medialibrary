d58124918ac1faf8325fb4139ace923479628300
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-05-29 10:08:04 +0200

    Add missing file

---
+++ test/unittest/MiscTests.cpp
@@ -0,0 +1,40 @@
+/*****************************************************************************
+ * Media Library
+ *****************************************************************************
+ * Copyright (C) 2017 Hugo Beauzée-Luyssen, Videolabs
+ *
+ * Authors: Hugo Beauzée-Luyssen<hugo@beauzee.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "Tests.h"
+
+class Misc : public Tests
+{
+};
+
+TEST_F( Misc, FileExtensions )
+{
+    const auto supportedExtensions = ml->getSupportedExtensions();
+    for ( auto i = 0u; i < supportedExtensions.size() - 1; i++ )
+    {
+        ASSERT_LT( strcmp( supportedExtensions[i], supportedExtensions[i + 1] ), 0 );
+    }
+}
