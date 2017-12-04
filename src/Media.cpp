b195b5dedb85c98393f6c5bbfc799a12836b3873
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-12-04 11:46:16 +0100

    Media: Ensure is_present is always 1 or 0
    (cherry picked from commit 41f045b93056a3bd2d272ea91a24521667a1501c)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- src/Media.cpp
+++ src/Media.cpp
@@ -555,7 +555,8 @@ bool Media::createTriggers( sqlite::Connection* connection )
             "is_present ON " + policy::FileTable::Name +
             " BEGIN "
             " UPDATE " + policy::MediaTable::Name + " SET is_present="
-                "(SELECT COUNT(id_file) FROM " + policy::FileTable::Name + " WHERE media_id=new.media_id AND is_present=1) "
+                "(SELECT COUNT(id_file) FROM " + policy::FileTable::Name +
+                    " WHERE media_id=new.media_id AND is_present=1 LIMIT 1) "
                 "WHERE id_media=new.media_id;"
             " END;";
     static const std::string triggerReq2 = "CREATE TRIGGER IF NOT EXISTS cascade_file_deletion AFTER DELETE ON "
