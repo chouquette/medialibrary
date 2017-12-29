9383d0422bf0fb05078ce91e8c660e473919fde9
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2017-12-29 16:13:41 +0100

    MetadataParser: Ensure we have a media to analyze
    (cherry picked from commit 12a45daae6222c880c66b643bf06c31eea4ce2d3)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- src/metadata_services/MetadataParser.cpp
+++ src/metadata_services/MetadataParser.cpp
@@ -99,6 +99,15 @@ parser::Task::Status MetadataParser::run( parser::Task& task )
         // Synchronize file step tracker with task
         task.markStepCompleted( static_cast<parser::Task::ParserStep>( task.step ) );
     }
+    else if ( task.media == nullptr )
+    {
+        // If we have a file but no media, this is a problem, we can analyze as
+        // much as we want, but won't be able to store anything.
+        // Keep in mind that if we are in this code path, we are not analyzing
+        // a playlist.
+        assert( false );
+        return parser::Task::Status::Fatal;
+    }
 
     const auto& tracks = task.vlcMedia.tracks();
 
