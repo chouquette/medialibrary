2d97d88f881b1c972a689b29889e86faa2a825b6
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-02-06 17:16:41 +0100

    Metadata: Common: Remove useless scope

--- src/metadata_services/vlc/Common.cpp
+++ src/metadata_services/vlc/Common.cpp
@@ -72,33 +72,31 @@ bool medialibrary::MetadataCommon::startPlayback( VLC::Media& media, VLC::MediaP
         });
     }
 
+    std::unique_lock<compat::Mutex> lock( mutex );
+    mp.play();
+    success = cond.wait_for( lock, std::chrono::seconds( 3 ), [&failedToStart, &hasAnyTrack]() {
+        return failedToStart == true || hasAnyTrack == true;
+    });
+
+    // In case the playback failed, we probably won't fetch anything interesting anyway.
+    if ( failedToStart == true || success == false )
+        return false;
+
+    // If we have any kind of track, but not a video track, we don't have to wait long, tracks are usually
+    // being discovered together.
+    if ( hasVideoTrack == false )
     {
-        std::unique_lock<compat::Mutex> lock( mutex );
-        mp.play();
-        success = cond.wait_for( lock, std::chrono::seconds( 3 ), [&failedToStart, &hasAnyTrack]() {
-            return failedToStart == true || hasAnyTrack == true;
-        });
-
-        // In case the playback failed, we probably won't fetch anything interesting anyway.
-        if ( failedToStart == true || success == false )
-            return false;
-
-        // If we have any kind of track, but not a video track, we don't have to wait long, tracks are usually
-        // being discovered together.
-        if ( hasVideoTrack == false )
+        if ( watchForArtworkChange == true )
         {
-            if ( watchForArtworkChange == true )
-            {
-                cond.wait_for( lock, std::chrono::milliseconds( 500 ), [&metaArtworkChanged]() {
-                    return metaArtworkChanged == true;
-                });
-            }
-            else
-            {
-                cond.wait_for( lock, std::chrono::seconds( 1 ), [&hasVideoTrack]() {
-                    return hasVideoTrack == true;
-                });
-            }
+            cond.wait_for( lock, std::chrono::milliseconds( 500 ), [&metaArtworkChanged]() {
+                return metaArtworkChanged == true;
+            });
+        }
+        else
+        {
+            cond.wait_for( lock, std::chrono::seconds( 1 ), [&hasVideoTrack]() {
+                return hasVideoTrack == true;
+            });
         }
     }
 
