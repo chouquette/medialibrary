5b4b3e91f5509bc5dca5c40794e5ca051ab8cb10
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-08-08 15:34:23 +0200

    Task: Split table creation in a specific file

---
+++ src/database/tables/Task_v14.sql
@@ -0,0 +1,18 @@
+"CREATE TABLE IF NOT EXISTS " + Task::Table::Name +
+"("
+    "id_task INTEGER PRIMARY KEY AUTOINCREMENT,"
+    "step INTEGER NOT NULL DEFAULT 0,"
+    "retry_count INTEGER NOT NULL DEFAULT 0,"
+    "mrl TEXT,"
+    "file_id UNSIGNED INTEGER,"
+    "parent_folder_id UNSIGNED INTEGER,"
+    "parent_playlist_id INTEGER,"
+    "parent_playlist_index UNSIGNED INTEGER,"
+    "UNIQUE(mrl, parent_playlist_id) ON CONFLICT FAIL,"
+    "FOREIGN KEY (parent_folder_id) REFERENCES " + Folder::Table::Name
+    + "(id_folder) ON DELETE CASCADE,"
+    "FOREIGN KEY (file_id) REFERENCES " + File::Table::Name
+    + "(id_file) ON DELETE CASCADE,"
+    "FOREIGN KEY (parent_playlist_id) REFERENCES " + Playlist::Table::Name
+    + "(id_playlist) ON DELETE CASCADE"
+")",
