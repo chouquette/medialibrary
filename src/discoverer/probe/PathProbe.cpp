chouquette/medialibrary

commit c355d7195b69fce2b9e128686ab8d97d281030e2

Don't use stripScheme to convert from mrl to path
This doesn't work when the mrl contains encoded characters.
This might hurt non-local path discovery at some point, but we don't
really handle it for now.

src/discoverer/probe/PathProbe.cpp
@@ -79,7 +79,7 @@ bool PathProbe::proceedOnDirectory( const fs::IDirectory& directory )
     }
     if ( m_splitPath.empty() == true )
         return true;
-    auto directoryPath = utils::file::stripScheme( directory.mrl() );
+    auto directoryPath = utils::file::toLocalPath( directory.mrl() );
 #ifndef _WIN32
     // In case we are discovering from "/", the root folder isn't part of the
     // splitted path stack, which would cause the prober to reject it.
@@ -94,15 +94,16 @@ bool PathProbe::proceedOnDirectory( const fs::IDirectory& directory )
 
 bool PathProbe::proceedOnFile( const fs::IFile& file )
 {
+    auto path = utils::file::toLocalPath( file.mrl() );
     if ( m_isDirectory == true && m_isDiscoveryEnded == false && m_splitPath.empty() == true )
     {
-        if ( utils::file::stripScheme( file.mrl() ) != m_path )
+        if ( path != m_path )
             return true;
         m_isDiscoveryEnded = true;
         return false;
     }
 
-    if ( m_path == utils::file::stripScheme( file.mrl() ) )
+    if ( m_path == path )
     {
         assert( m_splitPath.size() == 1 && file.name() == m_splitPath.top() );
         m_splitPath.pop();

src/metadata_services/MetadataParser.cpp
@@ -299,17 +299,17 @@ void MetadataAnalyzer::addPlaylistElement( IItem& item,
     if ( parentKnown == false && Folder::fromMrl( m_ml, entryPoint ) != nullptr )
     {
         auto probePtr = std::unique_ptr<prober::PathProbe>(
-                    new prober::PathProbe{ utils::file::stripScheme( mrl ),
+                    new prober::PathProbe{ utils::file::toLocalPath( mrl ),
                        isDirectory, std::move( playlistPtr ), parentFolder,
-                       utils::file::stripScheme( directoryMrl ), subitem.parentPlaylistIndex(), true } );
+                       utils::file::toLocalPath( directoryMrl ), subitem.parentPlaylistIndex(), true } );
         FsDiscoverer discoverer( fsFactory, m_ml, nullptr, std::move( probePtr ) );
         discoverer.reload( entryPoint );
         return;
     }
     auto probePtr = std::unique_ptr<prober::PathProbe>(
-                new prober::PathProbe{ utils::file::stripScheme( mrl ),
+                new prober::PathProbe{ utils::file::toLocalPath( mrl ),
                    isDirectory, std::move( playlistPtr ), parentFolder,
-                   utils::file::stripScheme( directoryMrl ), subitem.parentPlaylistIndex(), false } );
+                   utils::file::toLocalPath( directoryMrl ), subitem.parentPlaylistIndex(), false } );
     FsDiscoverer discoverer( fsFactory, m_ml, nullptr, std::move( probePtr ) );
     if ( parentKnown == false )
     {

commit 2911fe3a1296214f4fe1fc0d053185c90d98944e

PathProbe: Fix assertion
Regression introduced by c355d7195b69fce2b9e128686ab8d97d281030e2

src/discoverer/probe/PathProbe.cpp
@@ -27,6 +27,7 @@
 #include "discoverer/probe/PathProbe.h"
 
 #include "utils/Filename.h"
+#include "utils/Url.h"
 #include "medialibrary/filesystem/IDirectory.h"
 #include "medialibrary/filesystem/IFile.h"
 
@@ -105,7 +106,8 @@ bool PathProbe::proceedOnFile( const fs::IFile& file )
 
     if ( m_path == path )
     {
-        assert( m_splitPath.size() == 1 && file.name() == m_splitPath.top() );
+        assert( m_splitPath.size() == 1 &&
+                utils::url::decode( file.name() ) == m_splitPath.top() );
         m_splitPath.pop();
         m_isDiscoveryEnded = true;
         return true;