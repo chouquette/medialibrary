0c58e611bcaaad713e7d7e010b381b9c4c3ebd03
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2018-12-11 14:41:41 +0100

    sqlite: Connection: Remove misleading std::move
    std::weak_constructor takes a const shared_ptr& so no move will happen

--- src/database/SqliteConnection.cpp
+++ src/database/SqliteConnection.cpp
@@ -216,7 +216,7 @@ Connection::WeakDbContext::~WeakDbContext()
 
 Connection::ThreadSpecificConnection::ThreadSpecificConnection(
         std::shared_ptr<Connection> conn )
-    : m_weakConnection( std::move( conn ) )
+    : m_weakConnection( conn )
 {
 }
 
