1dc8eba7a13d94f2a370d0af7c8d2ffa89531592
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-01-22 13:18:53 +0100

    utils: Url: Add missing header guard
    (cherry picked from commit 3d2d674dad64c0f3301884868e9513bfe7b686e3)
    Signed-off-by: Hugo Beauzée-Luyssen <hugo@beauzee.fr>

--- src/utils/Url.h
+++ src/utils/Url.h
@@ -20,6 +20,8 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
+#pragma once
+
 #include <string>
 
 namespace medialibrary
