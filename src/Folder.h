4b0f982bb6f3588b303a741f9ae97b804c4d9996
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-03-11 11:41:49 +0100

    Folder: Fix typo

--- src/Folder.h
+++ src/Folder.h
@@ -76,7 +76,7 @@ public:
     virtual int64_t id() const override;
     virtual const std::string& mrl() const override;
     virtual const std::string& name() const override;
-    // Use for 13 -> 14 migration
+    // Used for 13 -> 14 migration
     void setName( std::string name );
     const std::string& rawMrl() const;
     void setMrl( std::string mrl );
