9101924ca4f2031a57ad7f57a5d6a5ae485ba4e9
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-05-28 14:25:43 +0200

    Album: Clarify meaning & expectations of some fields

--- src/Album.cpp
+++ src/Album.cpp
@@ -409,10 +409,15 @@ void Album::createTable( sqlite::Connection* dbConnection )
                 "release_year UNSIGNED INTEGER,"
                 "short_summary TEXT,"
                 "thumbnail_id UNSIGNED INT,"
+                // Number of tracks in this album, regardless of their presence
+                // state.
                 "nb_tracks UNSIGNED INTEGER DEFAULT 0,"
                 "duration UNSIGNED INTEGER NOT NULL DEFAULT 0,"
                 "nb_discs UNSIGNED INTEGER NOT NULL DEFAULT 1,"
-                "is_present UNSIGNED INTEGER NOT NULL DEFAULT 0,"
+                // The album presence state, which is the number of present tracks
+                // in this album
+                "is_present UNSIGNED INTEGER NOT NULL DEFAULT 0 "
+                    "CHECK(is_present <= nb_tracks),"
                 "FOREIGN KEY( artist_id ) REFERENCES " + Artist::Table::Name
                 + "(id_artist) ON DELETE CASCADE,"
                 "FOREIGN KEY(thumbnail_id) REFERENCES " + Thumbnail::Table::Name
