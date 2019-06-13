c3457d085fbe79bc7210b1851f1d9a9b9b324c4b
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-06-13 13:49:05 +0200

    Album: Split tables creation requests in their own sql file

--- src/Album.cpp
+++ src/Album.cpp
@@ -401,37 +401,11 @@ Query<IArtist> Album::artists( const QueryParameters* params ) const
 
 void Album::createTable( sqlite::Connection* dbConnection )
 {
-    const std::string req = "CREATE TABLE IF NOT EXISTS " +
-            Table::Name +
-            "("
-                "id_album INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "title TEXT COLLATE NOCASE,"
-                "artist_id UNSIGNED INTEGER,"
-                "release_year UNSIGNED INTEGER,"
-                "short_summary TEXT,"
-                "thumbnail_id UNSIGNED INT,"
-                // Number of tracks in this album, regardless of their presence
-                // state.
-                "nb_tracks UNSIGNED INTEGER DEFAULT 0,"
-                "duration UNSIGNED INTEGER NOT NULL DEFAULT 0,"
-                "nb_discs UNSIGNED INTEGER NOT NULL DEFAULT 1,"
-                // The album presence state, which is the number of present tracks
-                // in this album
-                "is_present UNSIGNED INTEGER NOT NULL DEFAULT 0 "
-                    "CHECK(is_present <= nb_tracks),"
-                "FOREIGN KEY( artist_id ) REFERENCES " + Artist::Table::Name
-                + "(id_artist) ON DELETE CASCADE,"
-                "FOREIGN KEY(thumbnail_id) REFERENCES " + Thumbnail::Table::Name
-                + "(id_thumbnail)"
-            ")";
-    const std::string vtableReq = "CREATE VIRTUAL TABLE IF NOT EXISTS "
-                + Table::Name + "Fts USING FTS3("
-                "title,"
-                "artist"
-            ")";
-
-    sqlite::Tools::executeRequest( dbConnection, req );
-    sqlite::Tools::executeRequest( dbConnection, vtableReq );
+    const std::string reqs[] = {
+        #include "database/tables/Album_v16.sql"
+    };
+    for ( const auto& req : reqs )
+        sqlite::Tools::executeRequest( dbConnection, req );
 }
 
 void Album::createTriggers( sqlite::Connection* dbConnection )
