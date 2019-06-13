58bedfccf321648764e5aedd88a0308fcfd21e67
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-06-13 13:42:46 +0200

    Artist: Split tables creation requests in their own sql file

--- src/Artist.cpp
+++ src/Artist.cpp
@@ -337,46 +337,11 @@ unsigned int Artist::nbTracks() const
 
 void Artist::createTable( sqlite::Connection* dbConnection )
 {
-    const std::string req = "CREATE TABLE IF NOT EXISTS " +
-            Artist::Table::Name +
-            "("
-                "id_artist INTEGER PRIMARY KEY AUTOINCREMENT,"
-                "name TEXT COLLATE NOCASE UNIQUE ON CONFLICT FAIL,"
-                "shortbio TEXT,"
-                "thumbnail_id TEXT,"
-                // Contains the number of albums where this artist is the album
-                // artist. Some or all albums might not be present.
-                "nb_albums UNSIGNED INT DEFAULT 0,"
-                // Contains the number of tracks by this artist, regardless of
-                // the album they appear on. Some or all tracks might not be present
-                "nb_tracks UNSIGNED INT DEFAULT 0,"
-                "mb_id TEXT,"
-                // A presence flag, that represents the number of present tracks.
-                // This is not linked to the album presence at all.
-                // An artist of which all tracks are not present is considered
-                // not present, even if one of its album contains a present
-                // track from another artist (the album will be present, however)
-                "is_present UNSIGNED INTEGER NOT NULL DEFAULT 0 "
-                    "CHECK (is_present <= nb_tracks),"
-                "FOREIGN KEY(thumbnail_id) REFERENCES " + Thumbnail::Table::Name
-                + "(id_thumbnail)"
-            ")";
-    const std::string reqRel = "CREATE TABLE IF NOT EXISTS MediaArtistRelation("
-                "media_id INTEGER NOT NULL,"
-                "artist_id INTEGER,"
-                "PRIMARY KEY (media_id, artist_id),"
-                "FOREIGN KEY(media_id) REFERENCES " + Media::Table::Name +
-                "(id_media) ON DELETE CASCADE,"
-                "FOREIGN KEY(artist_id) REFERENCES " + Artist::Table::Name + "("
-                    + Artist::Table::PrimaryKeyColumn + ") ON DELETE CASCADE"
-            ")";
-    const std::string reqFts = "CREATE VIRTUAL TABLE IF NOT EXISTS " +
-                Artist::Table::Name + "Fts USING FTS3("
-                "name"
-            ")";
-    sqlite::Tools::executeRequest( dbConnection, req );
-    sqlite::Tools::executeRequest( dbConnection, reqRel );
-    sqlite::Tools::executeRequest( dbConnection, reqFts );
+    const std::string reqs[] = {
+        #include "database/tables/Artist_v16.sql"
+    };
+    for ( const auto& req : reqs )
+        sqlite::Tools::executeRequest( dbConnection, req );
 }
 
 void Artist::createTriggers( sqlite::Connection* dbConnection, uint32_t dbModelVersion )
