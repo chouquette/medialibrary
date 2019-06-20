736dcb427b23af03f67a5aa99ec9b4eb0deb04a1
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-06-20 11:00:15 +0200

    MediaLibrary: Always pause the thumbnailer worker
    Whether we're using libvlc or not is irrelevent for this case.

--- src/MediaLibrary.cpp
+++ src/MediaLibrary.cpp
@@ -1454,10 +1454,8 @@ void MediaLibrary::pauseBackgroundOperations()
 {
     if ( m_parser != nullptr )
         m_parser->pause();
-#ifdef HAVE_LIBVLC
     if ( m_thumbnailer != nullptr )
         m_thumbnailer->pause();
-#endif
 }
 
 void MediaLibrary::resumeBackgroundOperations()
