acda038baa6fa523bfcb9c79189a9168f45bdc09
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-12-03 11:38:48 +0100

    IShow: Use a default destructor instead of an empty one

--- include/medialibrary/IShow.h
+++ include/medialibrary/IShow.h
@@ -31,7 +31,7 @@ namespace medialibrary
 class IShow
 {
     public:
-        virtual ~IShow() {}
+        virtual ~IShow() = default;
         virtual int64_t id() const = 0;
         virtual const std::string& title() const = 0;
         virtual time_t releaseDate() const = 0;
