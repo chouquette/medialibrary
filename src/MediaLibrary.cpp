093ac2a0edd79e0b29eab15b1114a78c156b0d9b
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2019-12-16 11:46:21 +0100

    migration: 21 -> 22: Remove unrequired trigger creation

--- src/MediaLibrary.cpp
+++ src/MediaLibrary.cpp
@@ -1729,8 +1729,6 @@ void MediaLibrary::migrateModel21to22()
     for ( const auto& req : reqs )
         sqlite::Tools::executeRequest( dbConn, req );
 
-    Media::createTriggers( dbConn, 22 );
-
     m_settings.setDbModelVersion( 22 );
     t->commit();
 }
