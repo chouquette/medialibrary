1bae2bd0e95a205e367c21814930e7ccef58ca81
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2020-01-15 09:32:52 +0100

    IDiscoverer: Cosmetics
    Use #pragma once for double inclusion check

--- include/discoverer/IDiscoverer.h
+++ include/discoverer/IDiscoverer.h
@@ -20,8 +20,7 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
  *****************************************************************************/
 
-#ifndef IDISCOVERER_H
-# define IDISCOVERER_H
+#pragma once
 
 #include <string>
 #include "Types.h"
@@ -45,5 +44,3 @@ public:
 };
 
 }
-
-#endif // IDISCOVERER_H
