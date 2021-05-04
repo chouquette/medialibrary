chouquette/medialibrary

commit ad6fac644c359f51d86277e3d9b88f5dc803361f

Folder: Rework folder removal
We can now convert the media contained in a removed folder to external
media instead of always deleting them.
Fix #297

src/Folder.cpp
@@ -38,6 +38,8 @@
 #include "utils/Filename.h"
 #include "utils/Url.h"
 
+#include <queue>
+
 namespace medialibrary
 {
 
@@ -377,19 +379,16 @@ bool Folder::excludeEntryFolder( MediaLibraryPtr ml, int64_t folderId )
     return sqlite::Tools::executeInsert( ml->getConn(), req, folderId ) != 0;
 }
 
-bool Folder::ban( MediaLibraryPtr ml, const std::string& mrl )
+bool Folder::ban( MediaLibraryPtr ml, const std::string& mrl,
+                  RemovalBehavior behavior )
 {
-    // Ensure we delete the existing folder if any & ban the folder in an "atomic" way
-    auto t = ml->getConn()->newTransaction();
-
     auto f = fromMrl( ml, mrl, BannedType::Any );
     if ( f != nullptr )
     {
         // No need to ban a folder twice
         if ( f->m_isBanned == true )
             return true;
-        // Let the foreign key destroy everything beneath this folder
-        destroy( ml, f->id() );
+        return remove( ml, std::move( f ), behavior );
     }
     auto fsFactory = ml->fsFactoryForMrl( mrl );
     if ( fsFactory == nullptr )
@@ -427,7 +426,6 @@ bool Folder::ban( MediaLibraryPtr ml, const std::string& mrl )
     auto res = sqlite::Tools::executeInsert( ml->getConn(), req, path,
                                              nullptr, true, device->id(),
                                              deviceFs->isRemovable() ) != 0;
-    t->commit();
     return res;
 }
 
@@ -594,6 +592,13 @@ std::shared_ptr<Device> Folder::device() const
     return m_device;
 }
 
+bool Folder::ban()
+{
+    const std::string req = "UPDATE " + Table::Name +
+            " SET is_banned = 1 WHERE id_folder = ?";
+    return sqlite::Tools::executeUpdate( m_ml->getConn(), req, m_id );
+}
+
 Query<IFolder> Folder::withMedia( MediaLibraryPtr ml, IMedia::Type type,
                                   const QueryParameters* params )
 {
@@ -628,6 +633,54 @@ Query<IFolder> Folder::entryPoints( MediaLibraryPtr ml, bool banned, int64_t dev
         return make_query<Folder, IFolder>( ml, "*", req, "", banned );
     req += " AND device_id = ?";
     return make_query<Folder, IFolder>( ml, "*", req, "", banned, deviceId );
+}
+
+bool Folder::remove( MediaLibraryPtr ml, std::shared_ptr<Folder> folder,
+                     RemovalBehavior behavior )
+{
+    if ( behavior == RemovalBehavior::RemovedFromDisk )
+    {
+        /* We expect a banned folder to no have any media linked with it */
+        assert( folder->isBanned() == false || folder->nbMedia() == 0 );
+        /*
+         * If we want to delete the media as well, we can just let the foreign
+         * keys delete everything
+         */
+        return DatabaseHelpers<Folder>::destroy( ml, folder->id() );
+    }
+    std::queue<std::shared_ptr<IFolder>> queue;
+    queue.push( folder );
+
+    /**
+     * Crawl through all the sub folders & convert the media to external
+     * Afterward, delete the folder, which will propagate to all subfolders
+     * through foreign keys
+     */
+    while ( queue.empty() == false )
+    {
+        auto f = std::move( queue.front() );
+        queue.pop();
+        auto subFolders = f->subfolders( nullptr )->all();
+        while ( subFolders.empty() == false )
+        {
+            queue.push( std::move( subFolders.back() ) );
+            subFolders.pop_back();
+        }
+        auto t = ml->getConn()->newTransaction();
+        auto subMedia = f->media( IMedia::Type::Unknown, nullptr )->all();
+        for ( const auto& media : subMedia )
+        {
+            auto m = static_cast<Media*>( media.get() );
+            if ( m->convertToExternal() == false )
+                return false;
+        }
+        t->commit();
+    }
+    /* If we're banning an entry point, we just need to delete if from the database */
+    if ( folder->isRootFolder() == true )
+        return DatabaseHelpers<Folder>::destroy( ml, folder->id() );
+    /* Otherwise we need to flag that it was banned so that it's not discovered again */
+    return folder->ban();
 }
 
 int64_t Folder::id() const

src/Folder.h
@@ -77,6 +77,20 @@ public:
         Any,    //< Well... any of the above.
     };
 
+    enum class RemovalBehavior
+    {
+        /*
+         * The folder was removed from disk: remove it from the db and delete
+         * the media it contained
+         */
+        RemovedFromDisk,
+        /*
+         * The entry point was explicitely removed. Flag it as such in database
+         * but keep its media on disk
+         */
+        EntrypointRemoved,
+    };
+
     Folder( MediaLibraryPtr ml, sqlite::Row& row );
     Folder( MediaLibraryPtr ml, const std::string& path, int64_t parent,
             int64_t deviceId , bool isRemovable );
@@ -94,7 +108,8 @@ public:
                                            int64_t parentId, const Device& device,
                                            fs::IDevice& deviceFs );
     static bool excludeEntryFolder( MediaLibraryPtr ml, int64_t folderId );
-    static bool ban( MediaLibraryPtr ml, const std::string& mrl );
+    static bool ban( MediaLibraryPtr ml, const std::string& mrl,
+                     RemovalBehavior behavior );
     static std::vector<std::shared_ptr<Folder>> fetchRootFolders( MediaLibraryPtr ml );
 
     static std::shared_ptr<Folder> fromMrl( MediaLibraryPtr ml,
@@ -109,6 +124,24 @@ public:
                                            const QueryParameters* params );
     static Query<IFolder> entryPoints( MediaLibraryPtr ml, bool banned,
                                        int64_t deviceId );
+    /**
+     * @brief deleteFolder Mark a folder in database as removed
+     * @param ml The media library instance
+     * @param folder The folder to remove
+     * @param behavior An enum member to drive the removal behavior
+     * @return true in case of success, false otherwise.
+     *
+     * This will mark the folder as removed. If the folder was an entry point,
+     * it will simply be removed from the database. If it was a sub folder of an
+     * entry point, the folder will be marked as banned in order to not be
+     * discovered again.
+     * If the behavior passed is RemovedFromDisk, all the media that belonged to
+     * that folder will be removed from the database. This also means that the
+     * media will be removed from any media group or playlist they belonged to.
+     * Otherwise, when behavior is EntrypointRemoved, the media will be converted
+     * to external media, and will be kept in database.
+     */
+    static bool remove( MediaLibraryPtr ml, std::shared_ptr<Folder> folder, RemovalBehavior behavior );
 
     virtual int64_t id() const override;
     virtual const std::string& mrl() const override;
@@ -145,6 +178,7 @@ private:
     static std::string filterByMediaType( IMedia::Type type );
 
     std::shared_ptr<Device> device() const;
+    bool ban();
 
 private:
     MediaLibraryPtr m_ml;

src/MediaLibrary.cpp
@@ -877,12 +877,6 @@ void MediaLibrary::onUpdatedFile( std::shared_ptr<File> file,
     }
 }
 
-bool MediaLibrary::deleteFolder( const Folder& folder )
-{
-    LOG_DEBUG( "deleting folder ", folder.mrl() );
-    return Folder::destroy( this, folder.id() );
-}
-
 LabelPtr MediaLibrary::createLabel( const std::string& label )
 {
     try

src/MediaLibrary.h
@@ -103,8 +103,6 @@ public:
                         std::shared_ptr<Folder> parentFolder,
                         std::shared_ptr<fs::IDirectory> parentFolderFs );
 
-    bool deleteFolder(const Folder& folder );
-
     virtual LabelPtr createLabel( const std::string& label ) override;
     virtual bool deleteLabel( LabelPtr label ) override;
 

src/discoverer/DiscovererWorker.cpp
@@ -460,26 +460,15 @@ void DiscovererWorker::runRemove( const std::string& ep )
         m_ml->getCb()->onEntryPointRemoved( ep, false );
         return;
     }
-    // The easy case is that this folder was directly discovered. In which case, we just
-    // have to delete it and it won't be discovered again.
-    // If it isn't, we also have to ban it to prevent it from reappearing. The Folder::banFolder
-    // method already handles the prior deletion
-    bool res;
-    if ( folder->isRootFolder() == false )
-        res = Folder::ban( m_ml, entryPoint );
-    else
-        res = m_ml->deleteFolder( *folder );
-    if ( res == false )
-    {
-        m_ml->getCb()->onEntryPointRemoved( ep, false );
-        return;
-    }
-    m_ml->getCb()->onEntryPointRemoved( ep, true );
+    auto res = Folder::remove( m_ml, std::move( folder ),
+                               Folder::RemovalBehavior::EntrypointRemoved );
+    m_ml->getCb()->onEntryPointRemoved( ep, res );
 }
 
 void DiscovererWorker::runBan( const std::string& entryPoint )
 {
-    auto res = Folder::ban( m_ml, entryPoint );
+    auto res = Folder::ban( m_ml, entryPoint,
+                            Folder::RemovalBehavior::RemovedFromDisk );
     m_ml->getCb()->onEntryPointBanned( entryPoint, res );
 }
 
@@ -492,7 +481,8 @@ void DiscovererWorker::runUnban( const std::string& entryPoint )
         m_ml->getCb()->onEntryPointUnbanned( entryPoint, false );
         return;
     }
-    auto res = m_ml->deleteFolder( *folder );
+    auto res = Folder::remove( m_ml, std::move( folder ),
+                                Folder::RemovalBehavior::RemovedFromDisk );
     m_ml->getCb()->onEntryPointUnbanned( entryPoint, res );
 
     auto parentPath = utils::file::parentDirectory( entryPoint );

src/discoverer/FsDiscoverer.cpp
@@ -136,7 +136,7 @@ bool FsDiscoverer::reloadFolder( std::shared_ptr<Folder> f,
         {
             LOG_DEBUG( "Failed to find folder matching entrypoint ", mrl, ". "
                       "Removing that folder" );
-            m_ml->deleteFolder( *f );
+            Folder::remove( m_ml, std::move( f ), Folder::RemovalBehavior::RemovedFromDisk );
             return false;
         }
     }
@@ -153,10 +153,12 @@ bool FsDiscoverer::reloadFolder( std::shared_ptr<Folder> f,
     return true;
 }
 
-void FsDiscoverer::checkRemovedDevices( fs::IDirectory& fsFolder, Folder& folder,
+void FsDiscoverer::checkRemovedDevices( fs::IDirectory& fsFolder,
+                                        std::shared_ptr<Folder> folder,
                                         fs::IFileSystemFactory& fsFactory,
                                         bool newFolder, bool rootFolder ) const
 {
+    assert( folder != nullptr );
     // Even when we're discovering a new folder, we want to rule out device removal as the cause of
     // an IO error. If this is the cause, simply abort the discovery. All the folder we have
     // discovered so far will be marked as non-present through sqlite hooks, and we'll resume the
@@ -204,7 +206,8 @@ void FsDiscoverer::checkRemovedDevices( fs::IDirectory& fsFolder, Folder& folder
     if ( newFolder == false )
     {
         // If we ever came across this folder, its content is now unaccessible: let's remove it.
-        m_ml->deleteFolder( folder );
+        Folder::remove( m_ml, std::move( folder ),
+                        Folder::RemovalBehavior::RemovedFromDisk );
     }
 }
 
@@ -296,7 +299,8 @@ void FsDiscoverer::checkFolder( std::shared_ptr<fs::IDirectory> currentFolderFs,
             {
                 LOG_INFO( "A .nomedia file was added into a known folder, "
                           "removing it." );
-                m_ml->deleteFolder( *currentFolder );
+                Folder::remove( m_ml, std::move( currentFolder ),
+                                 Folder::RemovalBehavior::RemovedFromDisk );
             }
             return;
         }
@@ -362,7 +366,7 @@ void FsDiscoverer::checkFolder( std::shared_ptr<fs::IDirectory> currentFolderFs,
             for ( const auto& f : subFoldersInDB )
             {
                 LOG_DEBUG( "Folder ", f->mrl(), " not found in FS, deleting it" );
-                m_ml->deleteFolder( *f );
+                Folder::remove( m_ml, f, Folder::RemovalBehavior::RemovedFromDisk );
             }
         }
         checkFiles( currentFolderFs, currentFolder, interruptProbe );
@@ -372,8 +376,8 @@ void FsDiscoverer::checkFolder( std::shared_ptr<fs::IDirectory> currentFolderFs,
     catch ( const fs::errors::System& ex )
     {
         LOG_WARN( "Failed to browse ", currentFolderFs->mrl(), ": ", ex.what() );
-        checkRemovedDevices( *currentFolderFs, *currentFolder, fsFactory,
-                             newFolder, rootFolder );
+        checkRemovedDevices( *currentFolderFs, std::move( currentFolder ),
+                             fsFactory, newFolder, rootFolder );
         // If the device has indeed been removed, fs::errors::DeviceRemoved will
         // be thrown, otherwise, we just failed to browse that folder and will
         // have to try again later.

src/discoverer/FsDiscoverer.h
@@ -62,10 +62,9 @@ private:
                     fs::IFileSystemFactory& fsFactory ) const;
     bool reloadFolder( std::shared_ptr<Folder> folder,
                        const IInterruptProbe& probe, fs::IFileSystemFactory& fsFactory );
-    void checkRemovedDevices( fs::IDirectory& fsFolder, Folder& folder,
+    void checkRemovedDevices(fs::IDirectory& fsFolder, std::shared_ptr<Folder> folder,
                               fs::IFileSystemFactory& fsFactory, bool newFolder,
                               bool rootFolder ) const;
-
 
 private:
     MediaLibrary* m_ml;

test/unittest/FolderTests.cpp
@@ -79,6 +79,24 @@ class Folders : public FoldersNoDiscover
         }
 };
 
+static void enforceFakeMediaTypes( MediaLibraryTester* ml )
+{
+    auto media = std::static_pointer_cast<Media>( ml->media(
+                                mock::FileSystemFactory::Root + "video.avi" ) );
+    media->setType( IMedia::Type::Video );
+    media->save();
+
+    media = std::static_pointer_cast<Media>( ml->media(
+                                    mock::FileSystemFactory::Root + "audio.mp3" ) );
+    media->setType( IMedia::Type::Audio );
+    media->save();
+
+    media = std::static_pointer_cast<Media>( ml->media(
+                                    mock::FileSystemFactory::SubFolder + "subfile.mp4" ) );
+    media->setType( IMedia::Type::Video );
+    media->save();
+}
+
 TEST_F( Folders, Add )
 {
     auto files = ml->files();
@@ -398,15 +416,41 @@ TEST_F( Folders, FetchEntryPoints )
 
 TEST_F( Folders, RemoveRootEntryPoint )
 {
+    enforceFakeMediaTypes( ml.get() );
     auto media = ml->files();
-    ASSERT_NE( 0u, media.size() );
+    ASSERT_EQ( 3u, media.size() );
+    auto video = ml->videoFiles( nullptr )->all();
+    ASSERT_EQ( 2u, video.size() );
+    auto audio = ml->audioFiles( nullptr )->all();
+    ASSERT_EQ( 1u, audio.size() );
+
+    auto m = ml->media( mock::FileSystemFactory::Root + "video.avi" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_FALSE( m->isExternalMedia() );
+    m = ml->media( mock::FileSystemFactory::SubFolder + "subfile.mp4" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_FALSE( m->isExternalMedia() );
 
     ml->removeEntryPoint( mock::FileSystemFactory::Root );
     auto res = cbMock->waitEntryPointRemoved();
     ASSERT_TRUE( res );
 
     media = ml->files();
-    ASSERT_EQ( 0u, media.size() );
+    ASSERT_EQ( 3u, media.size() );
+
+    video = ml->videoFiles( nullptr )->all();
+    ASSERT_EQ( 0u, video.size() );
+    audio = ml->audioFiles( nullptr )->all();
+    ASSERT_EQ( 0u, audio.size() );
+
+    /* The media should now be converted to an external media */
+    m = ml->media( mock::FileSystemFactory::Root + "video.avi" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_TRUE( m->isExternalMedia() );
+
+    m = ml->media( mock::FileSystemFactory::SubFolder + "subfile.mp4" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_TRUE( m->isExternalMedia() );
 
     auto eps = ml->entryPoints()->all();
     ASSERT_EQ( 0u, eps.size() );
@@ -417,6 +461,10 @@ TEST_F( Folders, RemoveEntryPoint )
     auto media = ml->files();
     ASSERT_NE( 0u, media.size() );
 
+    auto m = ml->media( mock::FileSystemFactory::SubFolder + "subfile.mp4" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_FALSE( m->isExternalMedia() );
+
     ml->removeEntryPoint( mock::FileSystemFactory::SubFolder );
     auto res = cbMock->waitEntryPointRemoved();
     ASSERT_TRUE( res );
@@ -426,6 +474,10 @@ TEST_F( Folders, RemoveEntryPoint )
 
     auto eps = ml->entryPoints()->all();
     ASSERT_EQ( 1u, eps.size() );
+
+    m = ml->media( mock::FileSystemFactory::SubFolder + "subfile.mp4" );
+    ASSERT_NE( nullptr, m );
+    ASSERT_TRUE( m->isExternalMedia() );
 
     ml->reload();
 
@@ -450,24 +502,6 @@ TEST_F( Folders, RemoveRootFolder )
     Reload();
 
     ASSERT_EQ( 0u, ml->files().size() );
-}
-
-static void enforceFakeMediaTypes( MediaLibraryTester* ml )
-{
-    auto media = std::static_pointer_cast<Media>( ml->media(
-                                mock::FileSystemFactory::Root + "video.avi" ) );
-    media->setType( IMedia::Type::Video );
-    media->save();
-
-    media = std::static_pointer_cast<Media>( ml->media(
-                                    mock::FileSystemFactory::Root + "audio.mp3" ) );
-    media->setType( IMedia::Type::Audio );
-    media->save();
-
-    media = std::static_pointer_cast<Media>( ml->media(
-                                    mock::FileSystemFactory::SubFolder + "subfile.mp4" ) );
-    media->setType( IMedia::Type::Video );
-    media->save();
 }
 
 TEST_F( Folders, NbMedia )

commit 74b68f799ec39a72873f01b5fdc1819d0ee5dea9

Folder: Fix folder ban
Regression introduced in ad6fac644c359f51d86277e3d9b88f5dc803361f
Fix #319

src/Folder.cpp
@@ -371,13 +371,16 @@ std::shared_ptr<Folder> Folder::create( MediaLibraryPtr ml, const std::string& m
 
 bool Folder::ban( MediaLibraryPtr ml, const std::string& mrl )
 {
+    auto t = ml->getConn()->newTransaction();
+
     auto f = fromMrl( ml, mrl, BannedType::Any );
     if ( f != nullptr )
     {
         // No need to ban a folder twice
         if ( f->m_isBanned == true )
             return true;
-        return remove( ml, std::move( f ), RemovalBehavior::RemovedFromDisk );
+        if ( remove( ml, std::move( f ), RemovalBehavior::RemovedFromDisk ) == false )
+            return false;
     }
     auto fsFactory = ml->fsFactoryForMrl( mrl );
     if ( fsFactory == nullptr )
@@ -415,6 +418,8 @@ bool Folder::ban( MediaLibraryPtr ml, const std::string& mrl )
     auto res = sqlite::Tools::executeInsert( ml->getConn(), req, path,
                                              nullptr, true, device->id(),
                                              deviceFs->isRemovable() ) != 0;
+    if ( res == true )
+        t->commit();
     return res;
 }
 