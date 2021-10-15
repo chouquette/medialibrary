de85c7619eccda1494c9b7a527b48ee8527ed95a
Hugo Beauzée-Luyssen <hugo@beauzee.fr>
2021-10-15 15:04:50 +0200

    benchmark: Add a minimal request benchmark

---
+++ benchmark/requests/BenchAlbumRequests.cpp
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Media Library
+ *****************************************************************************
+ * Copyright (C) 2021 Hugo Beauzée-Luyssen, Videolabs, VideoLAN
+ *
+ * Authors: Hugo Beauzée-Luyssen <hugo@beauzee.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "BenchRequestsCommon.h"
+#include <benchmark/benchmark.h>
+#include "Album.h"
+
+static void ListAllAlbums( benchmark::State& state )
+{
+    auto bml = commonInit();
+    QueryParameters params{};
+    params.sort = static_cast<SortingCriteria>( state.range( 0 ) );
+    for ( auto _ : state )
+    {
+        auto albums = bml.ml->albums( &params );
+        benchmark::DoNotOptimize( albums );
+    }
+}
+
+
+BENCHMARK( ListAllAlbums )
+    ->Arg( toInt( SortingCriteria::Artist ) )
+    ->Arg( toInt( SortingCriteria::ReleaseDate ) )
+    ->Arg( toInt( SortingCriteria::Duration ) )
+    ->Arg( toInt( SortingCriteria::TrackNumber ) )
+    ->Arg( toInt( SortingCriteria::PlayCount ) )
+    ->Arg( toInt( SortingCriteria::InsertionDate ) )
+    ->Arg( toInt( SortingCriteria::Default ) );
---
+++ benchmark/requests/BenchRequestsCommon.cpp
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ * Media Library
+ *****************************************************************************
+ * Copyright (C) 2021 Hugo Beauzée-Luyssen, Videolabs, VideoLAN
+ *
+ * Authors: Hugo Beauzée-Luyssen <hugo@beauzee.fr>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
+#if HAVE_CONFIG_H
+# include "config.h"
+#endif
+
+#include "BenchRequestsCommon.h"
+#include "utils/File.h"
+#include "medialibrary/filesystem/Errors.h"
+
+BenchMedialib commonInit()
+{
+    BenchMedialib bml;
+    auto db = "bench.db";
+    if ( utils::fs::fileSize( db ) == 0 )
+        abort();
+    bml.ml.reset(
+        NewMediaLibrary( db, "/tmp/bench_ml", false ) );
+    bml.ml->initialize( &bml.cb );
+    return bml;
+}
