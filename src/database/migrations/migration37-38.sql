3d6359dd085bbd528e436ed8694c1c7bc3705ee4
Alaric Senat <dev.asenat@posteo.net>
2024-02-01 15:54:21 +0100

    Subscription: Move `artwork_mrl` to db model 38
    Refs #481
    
    (cherry picked from commit ae1ea3ad79380da672373b415de88bb38b24ac92)

--- src/database/migrations/migration37-38.sql
+++ src/database/migrations/migration37-38.sql
@@ -1,3 +1,5 @@
+"ALTER TABLE " + Subscription::Table::Name + " ADD COLUMN artwork_mrl TEXT",
+
 "ALTER TABLE " + Playlist::Table::Name + " ADD COLUMN is_favorite BOOLEAN NOT NULL DEFAULT FALSE",
 "ALTER TABLE " + MediaGroup::Table::Name + " ADD COLUMN is_favorite BOOLEAN NOT NULL DEFAULT FALSE",
 "ALTER TABLE " + Folder::Table::Name + " ADD COLUMN is_favorite BOOLEAN NOT NULL DEFAULT FALSE",
