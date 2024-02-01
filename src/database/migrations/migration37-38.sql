a345092a0b73432041e48bad3d9b4776bc212b04
Alaric Senat <dev.asenat@posteo.net>
2024-02-01 15:49:45 +0100

    MediaGroup: Move `is_favorite` to db model 38
    Refs #481

--- src/database/migrations/migration37-38.sql
+++ src/database/migrations/migration37-38.sql
@@ -1,1 +1,2 @@
+"ALTER TABLE " + MediaGroup::Table::Name + " ADD COLUMN is_favorite BOOLEAN NOT NULL DEFAULT FALSE",
 "ALTER TABLE " + Folder::Table::Name + " ADD COLUMN is_favorite BOOLEAN NOT NULL DEFAULT FALSE",
